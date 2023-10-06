063cd3629c5be8db3088fdbfa69a6312bdefb7a7
roychen3 <roy850323@gmail.com>
2023-10-06 16:22:42 +0800

    WIP

--- example/src/components/index.tsx
+++ example/src/components/index.tsx
@@ -1,10 +1,13 @@
-import {
-  StyleProvider as MFUIStyleProvider,
-  Button as MFUIButton,
-} from 'micro-frontend-react-ui';
-import type { StyleProviderProps } from 'micro-frontend-react-ui/dist/StyleProvider';
-import type { ButtonProps } from 'micro-frontend-react-ui/dist/Button';
-
+// import {
+//   StyleProvider as MFUIStyleProvider,
+//   Button as MFUIButton,
+// } from 'micro-frontend-react-ui';
+import MFUIStyleProvider from 'micro-frontend-react-ui/dist/es/StyleProvider';
+import MFUIButton from 'micro-frontend-react-ui/dist/es/Button';
+import type { StyleProviderProps } from 'micro-frontend-react-ui/dist/es/StyleProvider';
+import type { ButtonProps } from 'micro-frontend-react-ui/dist/es/Button';
+// import { defaultTheme } from 'micro-frontend-react-ui/dist/es/useTheme';
+// console.log('defaultTheme', defaultTheme)
 export function StyleProvider(props: StyleProviderProps) {
   return <MFUIStyleProvider {...props} />;
 }
--- rollup.config.js
+++ rollup.config.js
@@ -4,25 +4,51 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-const packageJson = require('./package.json');
-
 export default [
   {
     input: 'src/index.ts',
-    output: [
-      {
-        format: 'umd',
-        sourcemap: true,
-        name: 'MicroFrontendReactUI',
-        file: packageJson.main,
-        // interop: 'auto', // fix `typeError: XXX is not a function` in jest env.
-      },
-    ],
+    output: {
+      format: 'cjs',
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      dir: 'dist/cjs',
+      interop: 'auto', // fix `typeError: XXX is not a function` in jest env.
+    },
     plugins: [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        compilerOptions: {
+          outDir: 'dist/cjs',
+        },
+      }),
+      terser(),
+    ],
+    external: ['@emotion/react', '@emotion/styled', 'react', 'react-dom'],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      format: 'es',
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      dir: 'dist/es',
+      interop: 'auto', // fix `typeError: XXX is not a function` in jest env.
+    },
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        compilerOptions: {
+          outDir: 'dist/es',
+        },
+      }),
       terser(),
     ],
     external: ['@emotion/react', '@emotion/styled', 'react', 'react-dom'],
